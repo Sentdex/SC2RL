26339d3b75fc75e7f96c63b8d6f5fcab7913cfba
LooseTerrifyingSpaceMonkey <steven.lohrenz@gmail.com>
2022-11-16 20:48:39 -0800

    Cleaned up the code and added some comments.

--- README.md
+++ README.md
@@ -4,4 +4,11 @@ Reinforcement Learning + Starcraft 2
 Trained model file: https://www.dropbox.com/s/k8bomuzapmxychm/models.zip?dl=0
 
 Project associated with the following video: https://youtu.be/q59wap1ELQ4
-
+con
+Steps: 
+1. Install opencv-python package
+2. Install burnysc2
+3. Install stable-baselines3
+4. Install tensorboard
+5. Create an account at wandb.ai
+6. Set WANDB_API_KEY environment variable to the api key from W and B. 
--- incredibot-sct.py
+++ incredibot-sct.py
@@ -1,7 +1,7 @@
 from sc2.bot_ai import BotAI  # parent class we inherit from
 from sc2.data import Difficulty, Race  # difficulty for bots, race for the 1 of 3 races
 from sc2.main import run_game  # function that facilitates actually running the agents in games
-from sc2.player import Bot, Computer  #wrapper for whether or not the agent is one of your bots, or a "computer" player
+from sc2.player import Bot, Computer  # wrapper for whether or not the agent is one of your bots, or a "computer" player
 from sc2 import maps  # maps method for loading maps to play in.
 from sc2.ids.unit_typeid import UnitTypeId
 import random
@@ -12,17 +12,15 @@ import sys
 import pickle
 import time
 
-
 SAVE_REPLAY = True
 
-total_steps = 10000 
+total_steps = 10000
 steps_for_pun = np.linspace(0, 1, total_steps)
-step_punishment = ((np.exp(steps_for_pun**3)/10) - 0.1)*10
+step_punishment = ((np.exp(steps_for_pun ** 3) / 10) - 0.1) * 10
 
 
-
-class IncrediBot(BotAI): # inhereits from BotAI (part of BurnySC2)
-    async def on_step(self, iteration: int): # on_step is a method that is called every step of the game.
+class IncrediBot(BotAI):  # inherits from BotAI (part of BurnySC2)
+    async def on_step(self, iteration: int):  # on_step is a method that is called every step of the game.
         no_action = True
         while no_action:
             try:
@@ -30,16 +28,15 @@ class IncrediBot(BotAI): # inhereits from BotAI (part of BurnySC2)
                     state_rwd_action = pickle.load(f)
 
                     if state_rwd_action['action'] is None:
-                        #print("No action yet")
+                        # print("No action yet")
                         no_action = True
                     else:
-                        #print("Action found")
+                        # print("Action found")
                         no_action = False
             except:
                 pass
 
-
-        await self.distribute_workers() # put idle workers back to work
+        await self.distribute_workers()  # put idle workers back to work
 
         action = state_rwd_action['action']
         '''
@@ -67,7 +64,7 @@ class IncrediBot(BotAI): # inhereits from BotAI (part of BurnySC2)
                     for nexus in self.townhalls:
                         # get worker count for this nexus:
                         worker_count = len(self.workers.closer_than(10, nexus))
-                        if worker_count < 22: # 16+3+3
+                        if worker_count < 22:  # 16+3+3
                             if nexus.is_idle and self.can_afford(UnitTypeId.PROBE):
                                 nexus.train(UnitTypeId.PROBE)
                                 found_something = True
@@ -89,8 +86,7 @@ class IncrediBot(BotAI): # inhereits from BotAI (part of BurnySC2)
             except Exception as e:
                 print(e)
 
-
-        #1: build stargate (or up to one) (evenly)
+        # 1: build stargate (or up to one) (evenly)
         elif action == 1:
             try:
                 # iterate thru all nexus and see if these buildings are close
@@ -101,11 +97,12 @@ class IncrediBot(BotAI): # inhereits from BotAI (part of BurnySC2)
                         if self.can_afford(UnitTypeId.GATEWAY) and self.already_pending(UnitTypeId.GATEWAY) == 0:
                             # build gateway
                             await self.build(UnitTypeId.GATEWAY, near=nexus)
-                        
+
                     # if the is not a cybernetics core close:
                     if not self.structures(UnitTypeId.CYBERNETICSCORE).closer_than(10, nexus).exists:
                         # if we can afford it:
-                        if self.can_afford(UnitTypeId.CYBERNETICSCORE) and self.already_pending(UnitTypeId.CYBERNETICSCORE) == 0:
+                        if self.can_afford(UnitTypeId.CYBERNETICSCORE) and self.already_pending(
+                                UnitTypeId.CYBERNETICSCORE) == 0:
                             # build cybernetics core
                             await self.build(UnitTypeId.CYBERNETICSCORE, near=nexus)
 
@@ -119,19 +116,18 @@ class IncrediBot(BotAI): # inhereits from BotAI (part of BurnySC2)
             except Exception as e:
                 print(e)
 
-
-        #2: build voidray (random stargate)
+        # 2: build VoidRay (random stargate)
         elif action == 2:
             try:
                 if self.can_afford(UnitTypeId.VOIDRAY):
                     for sg in self.structures(UnitTypeId.STARGATE).ready.idle:
                         if self.can_afford(UnitTypeId.VOIDRAY):
                             sg.train(UnitTypeId.VOIDRAY)
-            
+
             except Exception as e:
                 print(e)
 
-        #3: send scout
+        # 3: send scout
         elif action == 3:
             # are there any idle probes:
             try:
@@ -154,8 +150,7 @@ class IncrediBot(BotAI): # inhereits from BotAI (part of BurnySC2)
                 except Exception as e:
                     pass
 
-
-        #4: attack (known buildings, units, then enemy base, just go in logical order.)
+        # 4: attack (known buildings, units, then enemy base, just go in logical order.)
         elif action == 4:
             try:
                 # take all void rays and attack!
@@ -180,18 +175,17 @@ class IncrediBot(BotAI): # inhereits from BotAI (part of BurnySC2)
                     elif self.enemy_start_locations:
                         # attack!
                         voidray.attack(self.enemy_start_locations[0])
-            
+
             except Exception as e:
                 print(e)
-            
 
-        #5: voidray flee (back to base)
+        # 5: voidray flee (back to base)
         elif action == 5:
             if self.units(UnitTypeId.VOIDRAY).amount > 0:
                 for vr in self.units(UnitTypeId.VOIDRAY):
                     vr.attack(self.start_location)
 
-
+        # Construct the blank map
         map = np.zeros((self.game_info.map_size[0], self.game_info.map_size[1], 3), dtype=np.uint8)
 
         # draw the minerals:
@@ -200,11 +194,10 @@ class IncrediBot(BotAI): # inhereits from BotAI (part of BurnySC2)
             c = [175, 255, 255]
             fraction = mineral.mineral_contents / 1800
             if mineral.is_visible:
-                #print(mineral.mineral_contents)
-                map[math.ceil(pos.y)][math.ceil(pos.x)] = [int(fraction*i) for i in c]
+                # print(mineral.mineral_contents)
+                map[math.ceil(pos.y)][math.ceil(pos.x)] = [int(fraction * i) for i in c]
             else:
-                map[math.ceil(pos.y)][math.ceil(pos.x)] = [20,75,50]  
-
+                map[math.ceil(pos.y)][math.ceil(pos.x)] = [20, 75, 50]
 
         # draw the enemy start location:
         for enemy_start_location in self.enemy_start_locations:
@@ -218,8 +211,7 @@ class IncrediBot(BotAI): # inhereits from BotAI (part of BurnySC2)
             c = [100, 0, 255]
             # get unit health fraction:
             fraction = enemy_unit.health / enemy_unit.health_max if enemy_unit.health_max > 0 else 0.0001
-            map[math.ceil(pos.y)][math.ceil(pos.x)] = [int(fraction*i) for i in c]
-
+            map[math.ceil(pos.y)][math.ceil(pos.x)] = [int(fraction * i) for i in c]
 
         # draw the enemy structures:
         for enemy_structure in self.enemy_structures:
@@ -227,7 +219,7 @@ class IncrediBot(BotAI): # inhereits from BotAI (part of BurnySC2)
             c = [0, 100, 255]
             # get structure health fraction:
             fraction = enemy_structure.health / enemy_structure.health_max if enemy_structure.health_max > 0 else 0.0001
-            map[math.ceil(pos.y)][math.ceil(pos.x)] = [int(fraction*i) for i in c]
+            map[math.ceil(pos.y)][math.ceil(pos.x)] = [int(fraction * i) for i in c]
 
         # draw our structures:
         for our_structure in self.structures:
@@ -237,15 +229,14 @@ class IncrediBot(BotAI): # inhereits from BotAI (part of BurnySC2)
                 c = [255, 255, 175]
                 # get structure health fraction:
                 fraction = our_structure.health / our_structure.health_max if our_structure.health_max > 0 else 0.0001
-                map[math.ceil(pos.y)][math.ceil(pos.x)] = [int(fraction*i) for i in c]
-            
+                map[math.ceil(pos.y)][math.ceil(pos.x)] = [int(fraction * i) for i in c]
+
             else:
                 pos = our_structure.position
                 c = [0, 255, 175]
                 # get structure health fraction:
                 fraction = our_structure.health / our_structure.health_max if our_structure.health_max > 0 else 0.0001
-                map[math.ceil(pos.y)][math.ceil(pos.x)] = [int(fraction*i) for i in c]
-
+                map[math.ceil(pos.y)][math.ceil(pos.x)] = [int(fraction * i) for i in c]
 
         # draw the vespene geysers:
         for vespene in self.vespene_geyser:
@@ -259,39 +250,36 @@ class IncrediBot(BotAI): # inhereits from BotAI (part of BurnySC2)
             fraction = vespene.vespene_contents / 2250
 
             if vespene.is_visible:
-                map[math.ceil(pos.y)][math.ceil(pos.x)] = [int(fraction*i) for i in c]
+                map[math.ceil(pos.y)][math.ceil(pos.x)] = [int(fraction * i) for i in c]
             else:
-                map[math.ceil(pos.y)][math.ceil(pos.x)] = [50,20,75]
+                map[math.ceil(pos.y)][math.ceil(pos.x)] = [50, 20, 75]
 
         # draw our units:
         for our_unit in self.units:
             # if it is a voidray:
             if our_unit.type_id == UnitTypeId.VOIDRAY:
                 pos = our_unit.position
-                c = [255, 75 , 75]
+                c = [255, 75, 75]
                 # get health:
                 fraction = our_unit.health / our_unit.health_max if our_unit.health_max > 0 else 0.0001
-                map[math.ceil(pos.y)][math.ceil(pos.x)] = [int(fraction*i) for i in c]
-
+                map[math.ceil(pos.y)][math.ceil(pos.x)] = [int(fraction * i) for i in c]
 
             else:
                 pos = our_unit.position
                 c = [175, 255, 0]
                 # get health:
                 fraction = our_unit.health / our_unit.health_max if our_unit.health_max > 0 else 0.0001
-                map[math.ceil(pos.y)][math.ceil(pos.x)] = [int(fraction*i) for i in c]
+                map[math.ceil(pos.y)][math.ceil(pos.x)] = [int(fraction * i) for i in c]
 
         # show map with opencv, resized to be larger:
         # horizontal flip:
 
-        cv2.imshow('map',cv2.flip(cv2.resize(map, None, fx=4, fy=4, interpolation=cv2.INTER_NEAREST), 0))
+        cv2.imshow('map', cv2.flip(cv2.resize(map, None, fx=4, fy=4, interpolation=cv2.INTER_NEAREST), 0))
         cv2.waitKey(1)
 
         if SAVE_REPLAY:
             # save map image into "replays dir"
             cv2.imwrite(f"replays/{int(time.time())}-{iteration}.png", map)
-
-
 
         reward = 0
 
@@ -299,18 +287,17 @@ class IncrediBot(BotAI): # inhereits from BotAI (part of BurnySC2)
             attack_count = 0
             # iterate through our void rays:
             for voidray in self.units(UnitTypeId.VOIDRAY):
-                # if voidray is attacking and is in range of enemy unit:
+                # if VoidRay is attacking and is in range of enemy unit:
                 if voidray.is_attacking and voidray.target_in_range:
                     if self.enemy_units.closer_than(8, voidray) or self.enemy_structures.closer_than(8, voidray):
                         # reward += 0.005 # original was 0.005, decent results, but let's 3x it. 
-                        reward += 0.015  
+                        reward += 0.015
                         attack_count += 1
 
         except Exception as e:
-            print("reward",e)
+            print("reward", e)
             reward = 0
 
-        
         if iteration % 100 == 0:
             print(f"Iter: {iteration}. RWD: {reward}. VR: {self.units(UnitTypeId.VOIDRAY).amount}")
 
@@ -320,25 +307,21 @@ class IncrediBot(BotAI): # inhereits from BotAI (part of BurnySC2)
         with open('state_rwd_action.pkl', 'wb') as f:
             pickle.dump(data, f)
 
-        
-
 
 result = run_game(  # run_game is a function that runs the game.
-    maps.get("2000AtmospheresAIE"), # the map we are playing on
-    [Bot(Race.Protoss, IncrediBot()), # runs our coded bot, protoss race, and we pass our bot object 
-     Computer(Race.Zerg, Difficulty.Hard)], # runs a pre-made computer agent, zerg race, with a hard difficulty.
-    realtime=False, # When set to True, the agent is limited in how long each step can take to process.
+    maps.get("2000AtmospheresAIE"),  # the map we are playing on
+    [Bot(Race.Protoss, IncrediBot()),  # runs our coded bot, protoss race, and we pass our bot object
+     Computer(Race.Zerg, Difficulty.Hard)],  # runs a pre-made computer agent, zerg race, with a hard difficulty.
+    realtime=False,  # When set to True, the agent is limited in how long each step can take to process.
 )
-
 
 if str(result) == "Result.Victory":
     rwd = 500
 else:
     rwd = -500
 
-with open("results.txt","a") as f:
+with open("results.txt", "a") as f:
     f.write(f"{result}\n")
-
 
 map = np.zeros((224, 224, 3), dtype=np.uint8)
 observation = map
